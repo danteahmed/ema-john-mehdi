3b713a1b595f9f87e50e7139bada70638fcedcee
Mehedi Hassan <rehan51322@gmail.com>
2022-03-23 23:59:08 +0600

    grand total

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
+import './Cart.css'
 
-const Cart = ({cart}) => {
-   // const mind = props.cart;
+const Cart = ({ cart }) => {
+   // const {cart} = props;
+
+   let total = 0;
+   let shipping = 0;
+   for(const product of cart){
+      total = total + product.price;
+      shipping = shipping + product.shipping;
+   }
+   const tax = parseFloat((total * 10/ 100).toFixed(2));
+   const grandTotal = total + shipping + tax;
    return (
-      <div>
+      <div className='cart'>
          <h4>Order Summary</h4>
          <p>Selected Items: {cart.length}</p>
+         <p>Total Price: ${total}</p>
+         <p>Shipping: ${shipping}</p>
+         <p>Tax: {tax}</p>
+         <h5>Grand Total: {grandTotal}</h5>
       </div>
    );
 };
