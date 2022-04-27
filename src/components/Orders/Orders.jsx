681bd24e79c9b5de7f28639074d57bfbf2a2af07
Mehedi Hassan <rehan51322@gmail.com>
2022-04-27 16:36:51 +0600

    backend and pagination added

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -1,40 +1,40 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import useCart from '../../hooks/useCart';
-import useProducts from '../../hooks/useProducts';
-import { removeFromDb } from '../../utilities/fakedb';
-import Cart from '../Cart/Cart';
-import ReviewItem from '../ReviewItem/ReviewItem';
+import React from "react";
+import { Link } from "react-router-dom";
+import useCart from "../../hooks/useCart";
+import useProducts from "../../hooks/useProducts";
+import { removeFromDb } from "../../utilities/fakedb";
+import Cart from "../Cart/Cart";
+import ReviewItem from "../ReviewItem/ReviewItem";
 
 const Orders = () => {
-   const [products,setProducts] = useProducts();
+  const [products, setProducts] = useProducts();
+  const [cart, setCart] = useCart();
 
-   const [cart,setCart] = useCart(products);
-
-   const handleRemoveProduct = product =>{
-         const rest = cart.filter(pd => pd.id !== product.id);
-         setCart(rest);
-         removeFromDb(product.id)
-   }
-   return (
-      <div className='shop-container'>
-         <div className="review-container">
-            {
-               cart.map(product => <ReviewItem 
-                  key={product.id} 
-                  product={product}
-                  handleRemoveProduct={handleRemoveProduct}></ReviewItem>)
-            }
-         </div>
-         <div className="cart-container">
-            <Cart cart={cart}>
-               <Link to="/shipment">
-                  <button>Proceed Shipment</button>
-               </Link>
-            </Cart>
-         </div>
+  const handleRemoveProduct = (product) => {
+    const rest = cart.filter((pd) => pd._id !== product._id);
+    setCart(rest);
+    removeFromDb(product._id);
+  };
+  return (
+    <div className="shop-container">
+      <div className="review-container">
+        {cart.map((product) => (
+          <ReviewItem
+            key={product._id}
+            product={product}
+            handleRemoveProduct={handleRemoveProduct}
+          ></ReviewItem>
+        ))}
       </div>
-   );
+      <div className="cart-container">
+        <Cart cart={cart}>
+          <Link to="/shipment">
+            <button>Proceed Shipment</button>
+          </Link>
+        </Cart>
+      </div>
+    </div>
+  );
 };
 
 export default Orders;
--- src/components/Shipment/Shipment.jsx
+++ src/components/Shipment/Shipment.jsx
@@ -29,7 +29,7 @@ const Shipment = () => {
    return (
      <div className="form-container">
        <div>
-         <h2 className="form-title">Shipping Information</h2>
+         <h2 className="form-title">Your Shipping Info</h2>
          <form onSubmit={handleShipping}>
            <div className="input-group">
              <label htmlFor="name">Name</label>
